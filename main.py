a0ba5af8623ed3ac1e7531776213cd84126dc78f
Dhanya Francis <63131743+DanLizz@users.noreply.github.com>
2021-10-22 14:40:21 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Text-to-Morse Code Converter
+
+morse_dict = {
+    'A': '.-', 'B': '-...',
+    'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-',
+    'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-',
+    'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--',
+    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----', ', ': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-'
+}
+
+
+# Morse converter function
+def morse_converter(input_text):
+    morse_code = ''
+    for char in input_text:
+        if char != ' ':
+            morse_code += morse_dict[char] + ' '
+        elif char == ' ':
+            morse_code += ' '
+    return morse_code
+
+
+input_text = input("Enter a text: ").upper()
+morse_code = morse_converter(input_text)
+print(morse_code)
